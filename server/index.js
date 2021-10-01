157826ef463e52b7f4f9826fd83637d3e7c05d7a
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-10-01 16:52:50 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -3,24 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const authRouter = require("./routes/auth");
-const postRouter = require("./routes/post");
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Credentials",
-    true,
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,PUT,POST,DELETE,OPTIONS",
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json",
-  );
-  next();
-});
+const authRouter = require("./routers/auth");
+const postRouter = require("./routers/post");
+
 const connectDB = async () => {
   try {
     await mongoose.connect(
@@ -43,11 +28,26 @@ const connectDB = async () => {
 connectDB();
 
 const app = express();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Credentials",
+    true,
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,OPTIONS",
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json",
+  );
+  next();
+});
 app.use(express.json());
 app.use(
   cors({
-    origin:
-      "https://youthful-banach-a43e52.netlify.app",
+    origin: "*",
   }),
 );
 
