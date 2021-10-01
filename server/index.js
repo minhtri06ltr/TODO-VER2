857cc814fda89c51138465ed29d8966d8765ac15
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-10-01 12:33:49 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use(function (req, res, next) {
   // Website you wish to allow to connect
   res.setHeader(
     "Access-Control-Allow-Origin",
-    PORT,
+    "*",
   );
 
   // Request methods you wish to allow
