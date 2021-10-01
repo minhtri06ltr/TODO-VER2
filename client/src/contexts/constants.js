a92fc1414bad91ef95ec2c64f0a213c9ad56f392
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-10-01 10:18:40 +0700

    deploy

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,8 +1,8 @@
 //global variables
 export const apiUrl =
   process.env.NODE_ENV !== "production"
-    ? "https://shielded-atoll-53826.herokuapp.com/api"
-    : "somethinghere";
+    ? "http://localhost:5000/api"
+    : "https://shielded-atoll-53826.herokuapp.com/api";
 //name column in local storage
 export const LOCAL_STORAGE_TOKEN_NAME =
   "learn-mern";
