4dfb8b421d53d17d8a593dca0e1eb3a63b3073c9
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-10-01 11:06:12 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -1,16 +1,15 @@
-const express = require("express");
-const PORT = process.env.PORT || 5000;
-const app = express();
-const mongoose = require("mongoose");
-const authRouter = require("./routers/auth");
-const postRouter = require("./routers/post");
-const cors = require("cors");
 require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
+
 const connectDB = async () => {
   try {
-    // use package dotenv to use .env hide password,username database and other information
     await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mern-learn.xs1fh.mongodb.net/mern-learn?retryWrites=true&w=majority`,
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mern-learnit.j0fcc.mongodb.net/mern-learnit?retryWrites=true&w=majority`,
       {
         useCreateIndex: true,
         useNewUrlParser: true,
@@ -18,31 +17,25 @@ const connectDB = async () => {
         useFindAndModify: false,
       },
     );
-    console.log("Connect db successful");
+
+    console.log("MongoDB connected");
   } catch (error) {
     console.log(error.message);
-    process.exit(1); //exit  process
+    process.exit(1);
   }
 };
+
 connectDB();
 
-app.use(express.json()); //make express can read data in json type
+const app = express();
+app.use(express.json());
 app.use(cors());
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT",
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization",
-  );
-  next();
-});
-app.use("/api/posts", postRouter); //direct to routes/post
-app.use("/api/auth", authRouter); //direct to routes/auth
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
+
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server started on port ${PORT}`),
+);
