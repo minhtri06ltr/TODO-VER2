62c798bee0a911adbed27ec798c48c617124c9b8
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-10-01 11:32:18 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -26,9 +26,10 @@ const connectDB = async () => {
 };
 
 connectDB();
-app.options("*", cors());
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
