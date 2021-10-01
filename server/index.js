8da5d589c1944322a0e7a31988531d2bfe4fcf5f
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-10-01 12:22:46 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -30,7 +30,14 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", PORT); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 
