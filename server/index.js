f598fd4a7cddc26b85ed1f11cc59634c7f6f315b
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-10-01 11:38:39 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -30,7 +30,22 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    true,
+  );
+  res.header(
+    "Access-Control-Allow-Credentials",
+    true,
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+  );
+  next();
+});
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 
