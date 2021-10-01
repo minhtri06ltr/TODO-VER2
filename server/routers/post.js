5b7e36f720e9a94846047dacd1da4baf8d435478
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-10-01 12:41:32 +0700

    fix

--- server/routers/post.js
+++ server/routers/post.js
@@ -2,36 +2,7 @@ const express = require("express");
 const Post = require("../models/Post");
 const router = express.Router();
 const verifyToken = require("../middleware/auth");
-// Add headers
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "*",
-  );
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type",
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader(
-    "Access-Control-Allow-Credentials",
-    true,
-  );
-
-  // Pass to next layer of middleware
-  next();
-});
 //routers Put api/posts
 //@desc Delete post
 //@access private
