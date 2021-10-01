5ed0ccf2b74fead1084d6b5dcccc707fa3cbb3b2
minhtri06ltr <laptopdienthoai1@gmail.com>
2021-10-01 12:30:41 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -30,17 +30,34 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors());
+// Add headers
 app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
+  // Website you wish to allow to connect
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    PORT,
+  );
+
+  // Request methods you wish to allow
+  res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET,HEAD,OPTIONS,POST,PUT",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE",
   );
-  res.header(
+
+  // Request headers you wish to allow
+  res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization",
+    "X-Requested-With,content-type",
   );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader(
+    "Access-Control-Allow-Credentials",
+    true,
+  );
+
+  // Pass to next layer of middleware
   next();
 });
 app.use("/api/auth", authRouter);
